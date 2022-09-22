4fed3226c00f12cfc00c733288e0c5f96c075b6d
vokur13 <103522917+vokur13@users.noreply.github.com>
2022-09-22 11:27:22 +0000

    Deploying to gh-pages from @ vokur13/goit-js-practical-lessons@0093512d23f5f3ba7f04b5e5a8b0c8b059ff5c8e 🚀

--- index.a0c0d9e7.js
+++
@@ -1,2 +0,0 @@
-const{height:e}=document.querySelector(".header").getBoundingClientRect();document.body.style.paddingTop=`${e}px`;
-//# sourceMappingURL=index.a0c0d9e7.js.map
--- index.ad10831c.js
+++
@@ -1,2 +0,0 @@
-!function(){const{height:e}=document.querySelector(".header").getBoundingClientRect();document.body.style.paddingTop=`${e}px`}();
-//# sourceMappingURL=index.ad10831c.js.map
