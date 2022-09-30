769c20a7b76a024ee3b065f0b6c20e870cbf845b
vokur13 <vokur13@gmail.com>
2022-09-30 13:49:12 +0300

    instance added

--- src/js/drafts.js
+++ src/js/drafts.js
@@ -14,3 +14,17 @@ transactions.forEach(item => {
     console.log(element);
   }
 });
+
+// Фильтрация уникальных элементов
+
+// const students = [
+//   { name: "Манго", courses: ["математика", "физика"] },
+//   { name: "Поли", courses: ["информатика", "математика"] },
+//   { name: "Киви", courses: ["физика", "биология"] },
+// ];
+
+// const allCourses = students.flatMap(student => student.courses);
+// // ['математика', 'физика', 'информатика', 'математика', 'физика', 'биология'];
+// const uniqueCourses = allCourses.filter(
+//   (course, index, array) => array.indexOf(course) === index
+// );
